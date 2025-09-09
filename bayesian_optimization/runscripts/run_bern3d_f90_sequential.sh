2761380b2b7592b71b02182f9c71fe143aac38ec
Hofmann Elizondo Urs (uh24x373) <uh24x373@submit03.ubelix.unibe.ch>
2025-09-09 16:11:58 +0200

    Changed submission script for bayesian optimization

--- bayesian_optimization/runscripts/run_bern3d_f90_sequential.sh
+++ bayesian_optimization/runscripts/run_bern3d_f90_sequential.sh
@@ -6,7 +6,7 @@
 #SBATCH --qos=job_icpu-poeppelmeier
 #SBATCH --mem=2G
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 
 # Load modules
 module load intel/2023a
@@ -17,9 +17,10 @@ module load CMake/3.26.3-GCCcore-12.3.0
 ulimit -s unlimited
 unset SLURM_MEM_PER_CPU
 unset SLURM_MEM_PER_GPU
-#export OMP_STACKSIZE=32M
-#export OMP_PROC_BIND=close
-#export I_MPI_COMPATIBILITY=4
+
+export OMP_STACKSIZE=32M
+export OMP_PROC_BIND=close
+export I_MPI_COMPATIBILITY=4
 export HDF5_USE_FILE_LOCKING=FALSE
 
 # Run in serial mode
